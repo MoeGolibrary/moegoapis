d7225e36d3ad67d0ddc311bb7264fd86809dadf1
Young <zhangyang@moego.pet>
2024-07-26 14:58:56 +0800

    feat: add customerNote service ERP-12018

--- genproto/go/business/customer/v1/customerpb/customer_service_grpc.pb.go
+++ genproto/go/business/customer/v1/customerpb/customer_service_grpc.pb.go
@@ -19,10 +19,12 @@ import (
 const _ = grpc.SupportPackageIsVersion8
 
 const (
-	CustomerService_CreateCustomer_FullMethodName     = "/moego.business.customer.v1.CustomerService/CreateCustomer"
-	CustomerService_GetCustomer_FullMethodName        = "/moego.business.customer.v1.CustomerService/GetCustomer"
-	CustomerService_ListCustomers_FullMethodName      = "/moego.business.customer.v1.CustomerService/ListCustomers"
-	CustomerService_GenCustomerCofLink_FullMethodName = "/moego.business.customer.v1.CustomerService/GenCustomerCofLink"
+	CustomerService_CreateCustomer_FullMethodName      = "/moego.business.customer.v1.CustomerService/CreateCustomer"
+	CustomerService_GetCustomer_FullMethodName         = "/moego.business.customer.v1.CustomerService/GetCustomer"
+	CustomerService_ListCustomers_FullMethodName       = "/moego.business.customer.v1.CustomerService/ListCustomers"
+	CustomerService_GenCustomerCofLink_FullMethodName  = "/moego.business.customer.v1.CustomerService/GenCustomerCofLink"
+	CustomerService_AppendCustomerNotes_FullMethodName = "/moego.business.customer.v1.CustomerService/AppendCustomerNotes"
+	CustomerService_ListCustomerNotes_FullMethodName   = "/moego.business.customer.v1.CustomerService/ListCustomerNotes"
 )
 
 // CustomerServiceClient is the client API for CustomerService service.
@@ -39,6 +41,10 @@ type CustomerServiceClient interface {
 	ListCustomers(ctx context.Context, in *ListCustomersRequest, opts ...grpc.CallOption) (*ListCustomersResponse, error)
 	// Generate a link to add customer card on file
 	GenCustomerCofLink(ctx context.Context, in *GenCustomerCofLinkRequest, opts ...grpc.CallOption) (*GenCustomerCofLinkResponse, error)
+	// Batch Append notes to a Customer
+	AppendCustomerNotes(ctx context.Context, in *AppendCustomerNotesRequest, opts ...grpc.CallOption) (*AppendCustomerNotesResponse, error)
+	// List Customer Notes
+	ListCustomerNotes(ctx context.Context, in *ListCustomerNotesRequest, opts ...grpc.CallOption) (*ListCustomerNotesResponse, error)
 }
 
 type customerServiceClient struct {
@@ -89,6 +95,26 @@ func (c *customerServiceClient) GenCustomerCofLink(ctx context.Context, in *GenC
 	return out, nil
 }
 
+func (c *customerServiceClient) AppendCustomerNotes(ctx context.Context, in *AppendCustomerNotesRequest, opts ...grpc.CallOption) (*AppendCustomerNotesResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(AppendCustomerNotesResponse)
+	err := c.cc.Invoke(ctx, CustomerService_AppendCustomerNotes_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *customerServiceClient) ListCustomerNotes(ctx context.Context, in *ListCustomerNotesRequest, opts ...grpc.CallOption) (*ListCustomerNotesResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(ListCustomerNotesResponse)
+	err := c.cc.Invoke(ctx, CustomerService_ListCustomerNotes_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // CustomerServiceServer is the server API for CustomerService service.
 // All implementations must embed UnimplementedCustomerServiceServer
 // for forward compatibility
@@ -103,6 +129,10 @@ type CustomerServiceServer interface {
 	ListCustomers(context.Context, *ListCustomersRequest) (*ListCustomersResponse, error)
 	// Generate a link to add customer card on file
 	GenCustomerCofLink(context.Context, *GenCustomerCofLinkRequest) (*GenCustomerCofLinkResponse, error)
+	// Batch Append notes to a Customer
+	AppendCustomerNotes(context.Context, *AppendCustomerNotesRequest) (*AppendCustomerNotesResponse, error)
+	// List Customer Notes
+	ListCustomerNotes(context.Context, *ListCustomerNotesRequest) (*ListCustomerNotesResponse, error)
 	mustEmbedUnimplementedCustomerServiceServer()
 }
 
@@ -121,6 +151,12 @@ func (UnimplementedCustomerServiceServer) ListCustomers(context.Context, *ListCu
 }
 func (UnimplementedCustomerServiceServer) GenCustomerCofLink(context.Context, *GenCustomerCofLinkRequest) (*GenCustomerCofLinkResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GenCustomerCofLink not implemented")
+}
+func (UnimplementedCustomerServiceServer) AppendCustomerNotes(context.Context, *AppendCustomerNotesRequest) (*AppendCustomerNotesResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method AppendCustomerNotes not implemented")
+}
+func (UnimplementedCustomerServiceServer) ListCustomerNotes(context.Context, *ListCustomerNotesRequest) (*ListCustomerNotesResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ListCustomerNotes not implemented")
 }
 func (UnimplementedCustomerServiceServer) mustEmbedUnimplementedCustomerServiceServer() {}
 
@@ -207,6 +243,42 @@ func _CustomerService_GenCustomerCofLink_Handler(srv interface{}, ctx context.Co
 	return interceptor(ctx, in, info, handler)
 }
 
+func _CustomerService_AppendCustomerNotes_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(AppendCustomerNotesRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CustomerServiceServer).AppendCustomerNotes(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: CustomerService_AppendCustomerNotes_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CustomerServiceServer).AppendCustomerNotes(ctx, req.(*AppendCustomerNotesRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _CustomerService_ListCustomerNotes_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ListCustomerNotesRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CustomerServiceServer).ListCustomerNotes(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: CustomerService_ListCustomerNotes_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CustomerServiceServer).ListCustomerNotes(ctx, req.(*ListCustomerNotesRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // CustomerService_ServiceDesc is the grpc.ServiceDesc for CustomerService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -229,6 +301,14 @@ var CustomerService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GenCustomerCofLink",
 			Handler:    _CustomerService_GenCustomerCofLink_Handler,
+		},
+		{
+			MethodName: "AppendCustomerNotes",
+			Handler:    _CustomerService_AppendCustomerNotes_Handler,
+		},
+		{
+			MethodName: "ListCustomerNotes",
+			Handler:    _CustomerService_ListCustomerNotes_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
